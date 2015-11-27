754842fc7d88dfbb9b22e384f7c8d6534c88fd99
AlexCrosthwaite <abcrosthwaite@ucla.edu>
2015-11-26 17:45:12 -0800

    cast blocks to int*

--- ospfsmod.c
+++ ospfsmod.c
@@ -829,7 +829,7 @@ add_block(ospfs_inode_t *oi)
 		else
 		{
 			// We allocated an indirect block in the doubly indirect
-			ospfs_block(oi->oi_indirect2)[indir_index(n - 1)] = allocated[0];
+			((uint32_t*) ospfs_block(oi->oi_indirect2))[indir_index(n - 1)] = allocated[0];
 		}
 	}
 	else
@@ -838,12 +838,12 @@ add_block(ospfs_inode_t *oi)
 		if (indir2_index(n) == -1)
 		{
 			// In the singly indirect
-			ospfs_block(oi->oi_indirect)[direct_index(n - 1)] = newblock_no;
+			((uint32_t*) ospfs_block(oi->oi_indirect))[direct_index(n - 1)] = newblock_no;
 		}
 		else
 		{
 			// In the doubly indirect
-			ospfs_block(ospfs_block(oi->oi_indirect2)[indir_index(n - 1)])[direct_index(n - 1)] = newblock_no;
+			((uint32_t*) ospfs_block(((uint32_t*) ospfs_block(oi->oi_indirect2))[indir_index(n - 1)]))[direct_index(n - 1)] = newblock_no;
 		}
 	}
 	oi->oi_size += OSPFS_BLKSIZE;
